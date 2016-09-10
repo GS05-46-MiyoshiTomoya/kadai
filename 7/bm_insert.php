2bc8a3825bb539401e03169c22479d5d2e1e89f5
Miyoshi Tomoya <mcmht507@miyoshitomoya-no-MacBook-Pro.local>
2016-09-10 17:37:33 +0900

    基本的なところまでコミット

---
+++ 7/bm_insert.php
@@ -0,0 +1,37 @@
+<?php
+//1. POSTデータ取得
+$book_name = $_POST["book_name"];
+$book_url = $_POST["book_url"];
+$book_cmt = $_POST["book_cmt"];
+
+//2. DB接続します
+try {
+  $pdo = new PDO('mysql:dbname=gs_db;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('DbConnectError:'.$e->getMessage());
+}
+
+//３．データ登録SQL作成
+// $stmt = $pdo->prepare("INSERT INTO gs_an_table(id, name, email, naiyou,
+// indate )VALUES(NULL, :a1, :a2, :a3, :a4,sysdate())");
+
+$stmt = $pdo->prepare("INSERT INTO `gs_bm_table`(id, book_name, book_url, book_cmt,indate )VALUES(NULL, :a1, :a2, :a3,sysdate())");
+$stmt->bindValue(':a1', $book_name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a2', $book_url, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a3', $book_cmt, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+
+// データ実行に失敗した場合は＜False
+$status = $stmt->execute();
+//４．データ登録処理後
+var_dump($status);
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("QueryError:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+  header("Location: bm_insert_view.php");
+  exit;
+
+}
+?>
